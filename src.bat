5e19e04f5cadde7bd5237f0c08600937ba1fca11
Luuk3333 <Luuk3333@users.noreply.github.com>
2019-01-14 16:28:05 +0100

    Remove unused '.bat' files

--- src.bat
+++
@@ -1,3 +0,0 @@
-copy src_vs2013\FFA320Connector.cpp src\FFA320Connector.cpp
-copy src_vs2013\FFA320Connector.cpp src_xcode3.2\FFA320Connector.cpp
-copy src_xcode3.2\DerivedData\FFA320Connector\Build\Products\Release\mac.xpl bin\FFA320Connector\64\mac.xpl
--- xcopyplugin.bat
+++
@@ -1,1 +0,0 @@
-xcopy /e/Y/I "bin\FFA320Connector" "G:\X-Plane 11\Aircraft\My\FFA320\plugins\FFA320Connector"
